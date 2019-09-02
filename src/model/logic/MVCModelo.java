b7dfb206a81eca3039e47b7c85aa0fe1e6d62338
s.morenom <s.morenom@uniandes.edu.co>
2019-09-02 18:23:58 -0500

    Update cargar

--- src/model/logic/MVCModelo.java
+++ src/model/logic/MVCModelo.java
@@ -17,8 +17,12 @@ import model.data_structures.Viaje;
  *
  */
 public class MVCModelo {
-	private Queue cola;
-	private Stack pila;
+	private Queue colaM;
+	private Stack pilaM;
+	private Queue colaS;
+	private Stack pilaS;
+	private Queue colaH;
+	private Stack pilaH;
 
 	private boolean valor=true;
 	private Viaje agregar;
@@ -27,16 +31,51 @@ public class MVCModelo {
 	 * Constructor del modelo del mundo con capacidad predefinida
 	 */
 	// por alguna razón, no carga correctamente el excel que se nos entrega. Pero, si utilizamos uno con el mismo orden y tipo de datos pero que solo tiene mil lineas,funciona correctamente
-	public MVCModelo(String pRuta) throws Exception
-	{   cola= new Queue(null);
-	pila= new Stack(null);
+	public MVCModelo(int trimestre) throws Exception{
+	String pRutaM ="";
+	String pRutaS="";
+	String pRutaH="";
+	if(trimestre==1){
+		pRutaM=".data/bogota-cadastral-2018-1-MonthlyAggregate.csv";
+	    pRutaH=".data/bogota-cadastral-2018-1-HourlyAggregate.csv";
+       
+	    pRutaS=".data/bogota-cadastral-2018-1-WeeklyAggregate.csv";
+	}else if(trimestre==2){
+		pRutaM=".data/bogota-cadastral-2018-2-MonthlyAggregate.csv";
+	    pRutaH=".data/bogota-cadastral-2018-2-HourlyAggregate.csv";
+       
+	    pRutaS=".data/bogota-cadastral-2018-2-WeeklyAggregate.csv";
+	}else if(trimestre==3){
+		pRutaM=".data/bogota-cadastral-2018-3-MonthlyAggregate.csv";
+	    pRutaH=".data/bogota-cadastral-2018-3-HourlyAggregate.csv";
+       
+	    pRutaS=".data/bogota-cadastral-2018-3-WeeklyAggregate.csv";
+	}else if(trimestre==4){
+		pRutaM=".data/bogota-cadastral-2018-4-MonthlyAggregate.csv";
+	    pRutaH=".data/bogota-cadastral-2018-4-HourlyAggregate.csv";
+       
+	    pRutaS=".data/bogota-cadastral-2018-4-WeeklyAggregate.csv";
+	}
+	  colaM= new Queue(null);
+	      pilaM= new Stack(null);
+	   colaH =new Queue(null);
+		pilaH= new Stack(null);
+		   colaS= new Queue(null);
+			pilaS= new Stack(null);
 	CSVReader reader = null;
 	CSVReader reader2 = null;
+	CSVReader reader3 = null;
+	CSVReader reader4 = null;
+	CSVReader reader5= null;
+	CSVReader reader6 = null;
+	int min=0;
+	int max=0;
+
 
 	try {
-		reader= new CSVReader(new FileReader(pRuta));
-		reader2= new CSVReader(new FileReader(pRuta));
-		int c =0;
+		reader= new CSVReader(new FileReader(pRutaM));
+		reader2= new CSVReader(new FileReader(pRutaM));
+		int M =0;
 
 		for(String[] nextLine : reader) {
 			if(nextLine.toString().contains("sourceid,dstid,month,mean_travel_time,standard_deviation_travel_time,geometric_mean_travel_time,geometric_standard_deviation_travel_time"))
@@ -54,14 +93,23 @@ public class MVCModelo {
 				double desviacionEstandar=Double.parseDouble(nextLine[4]);
 				double tiempoPromedioGEnSegundos=Double.parseDouble(nextLine[5]);
 				double desviacionEstandarG=Double.parseDouble(nextLine[6]);
+				if(M==0){
+					min=inicioID;
+					max=inicioID;
+					}
+				if(inicioID>min)
+					min=inicioID;
+				if(inicioID>max)
+					max=inicioID;
 
 				Viaje i = new Viaje(inicioID,destinoID,hora,tiempoPromedioEnSegundos,desviacionEstandar,tiempoPromedioGEnSegundos,desviacionEstandarG,null);
 				agregar = i;
 
-				cola.enQueue(i);
-				c++;
+				colaM.enQueue(i);
+				M++;
 			}
 		}
+		System.out.print("El número de lineas leídas en el documento de viajes basado en mes del año del trimestre "+trimestre+" es: "+M);
 
 
 
@@ -80,12 +128,125 @@ public class MVCModelo {
 				double desviacionEstandar=Double.parseDouble(nextLine[4]);
 				double tiempoPromedioGEnSegundos=Double.parseDouble(nextLine[5]);
 				double desviacionEstandarG=Double.parseDouble(nextLine[6]);
-
+				if(inicioID>min)
+					min=inicioID;
+				if(inicioID>max)
+					max=inicioID;
 				Viaje i = new Viaje(inicioID,destinoID,hora,tiempoPromedioEnSegundos,desviacionEstandar,tiempoPromedioGEnSegundos,desviacionEstandarG,null);
 				agregar = i;
 
-				pila.push(i);
+				pilaM.push(i);
+			}}
+			reader3= new CSVReader(new FileReader(pRutaS));
+			reader4= new CSVReader(new FileReader(pRutaS));
+			int S =0;
+
+			for(String[] nextLine : reader3) {
+				if(nextLine.toString().contains("sourceid,dstid,dow,mean_travel_time,standard_deviation_travel_time,geometric_mean_travel_time,geometric_standard_deviation_travel_time"))
+				{
+
+				}
+				else
+				{
+
+
+					int  inicioID=Integer.parseInt(nextLine[0]);
+					int destinoID=Integer.parseInt(nextLine[1]);
+					int hora=Integer.parseInt(nextLine[2]);
+					double tiempoPromedioEnSegundos=Double.parseDouble(nextLine[3]);
+					double desviacionEstandar=Double.parseDouble(nextLine[4]);
+					double tiempoPromedioGEnSegundos=Double.parseDouble(nextLine[5]);
+					double desviacionEstandarG=Double.parseDouble(nextLine[6]);
+					if(inicioID>min)
+						min=inicioID;
+					if(inicioID>max)
+						max=inicioID;
+					Viaje i = new Viaje(inicioID,destinoID,hora,tiempoPromedioEnSegundos,desviacionEstandar,tiempoPromedioGEnSegundos,desviacionEstandarG,null);
+					agregar = i;
+
+					colaS.enQueue(i);
+					S++;
+				}
 			}
+			System.out.print("El número de lineas leídas en el documento de viajes basado en día de la semana del trimestre "+trimestre+" es: "+S);
+
+
+
+
+			for(String[] nextLine : reader4) {
+				if(nextLine.toString().contains("sourceid,dstid,dow,mean_travel_time,standard_deviation_travel_time,geometric_mean_travel_time,geometric_standard_deviation_travel_time"))
+				{
+
+				}
+				else
+				{
+					int  inicioID=Integer.parseInt(nextLine[0]);
+					int destinoID=Integer.parseInt(nextLine[1]);
+					int hora=Integer.parseInt(nextLine[2]);
+					double tiempoPromedioEnSegundos=Double.parseDouble(nextLine[3]);
+					double desviacionEstandar=Double.parseDouble(nextLine[4]);
+					double tiempoPromedioGEnSegundos=Double.parseDouble(nextLine[5]);
+					double desviacionEstandarG=Double.parseDouble(nextLine[6]);
+
+					Viaje i = new Viaje(inicioID,destinoID,hora,tiempoPromedioEnSegundos,desviacionEstandar,tiempoPromedioGEnSegundos,desviacionEstandarG,null);
+					agregar = i;
+
+					pilaS.push(i);
+				}
+		}
+			reader5= new CSVReader(new FileReader(pRutaS));
+			reader6= new CSVReader(new FileReader(pRutaS));
+			int H =0;
+
+			for(String[] nextLine : reader5) {
+				if(nextLine.toString().contains("sourceid,dstid,hod,mean_travel_time,standard_deviation_travel_time,geometric_mean_travel_time,geometric_standard_deviation_travel_time"))
+				{
+
+				}
+				else
+				{
+
+
+					int  inicioID=Integer.parseInt(nextLine[0]);
+					int destinoID=Integer.parseInt(nextLine[1]);
+					int hora=Integer.parseInt(nextLine[2]);
+					double tiempoPromedioEnSegundos=Double.parseDouble(nextLine[3]);
+					double desviacionEstandar=Double.parseDouble(nextLine[4]);
+					double tiempoPromedioGEnSegundos=Double.parseDouble(nextLine[5]);
+					double desviacionEstandarG=Double.parseDouble(nextLine[6]);
+
+					Viaje i = new Viaje(inicioID,destinoID,hora,tiempoPromedioEnSegundos,desviacionEstandar,tiempoPromedioGEnSegundos,desviacionEstandarG,null);
+					agregar = i;
+
+					colaH.enQueue(i);
+					H++;
+				}
+			}
+			System.out.print("El número de lineas leídas en el documento de viajes basados en la hora del día del trimestre "+trimestre+" es: "+H);
+
+
+
+
+			for(String[] nextLine : reader6) {
+				if(nextLine.toString().contains("sourceid,dstid,hod,mean_travel_time,standard_deviation_travel_time,geometric_mean_travel_time,geometric_standard_deviation_travel_time"))
+				{
+
+				}
+				else
+				{
+					int  inicioID=Integer.parseInt(nextLine[0]);
+					int destinoID=Integer.parseInt(nextLine[1]);
+					int hora=Integer.parseInt(nextLine[2]);
+					double tiempoPromedioEnSegundos=Double.parseDouble(nextLine[3]);
+					double desviacionEstandar=Double.parseDouble(nextLine[4]);
+					double tiempoPromedioGEnSegundos=Double.parseDouble(nextLine[5]);
+					double desviacionEstandarG=Double.parseDouble(nextLine[6]);
+
+					Viaje i = new Viaje(inicioID,destinoID,hora,tiempoPromedioEnSegundos,desviacionEstandar,tiempoPromedioGEnSegundos,desviacionEstandarG,null);
+					agregar = i;
+
+					pilaH.push(i);
+				}
 		}
 	} catch (FileNotFoundException e) {
 		e.printStackTrace();
@@ -100,7 +261,8 @@ public class MVCModelo {
 			}
 		}
 	}
-	System.out.print("-------- /nID zona de inicio:"+agregar.darInicioID()+"-------- /nID Destino:"+agregar.darDestinoID()+"-------- /nHora del dia:"+agregar.darHora()+"-------- /nTiempo Promedio:"+agregar.darTiempoPromedioEnSegundos());
+	System.out.print("La zona con el mayor identificador es la zona: "+max);
+	System.out.print("La zona con el menor identificador es la zona: "+min);
 	}
 
 }

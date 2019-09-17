3aa5c3a004dc3fb555cacde6e757886fc72a4958
s.morenom <s.morenom@uniandes.edu.co>
2019-09-16 20:42:59 -0500

    Update Controller

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -2,6 +2,7 @@ package controller;
 
 import java.util.Scanner;
 
+import model.data_structures.Node;
 import model.data_structures.Queue;
 import model.data_structures.Viaje;
 import model.logic.MVCModelo;
@@ -23,7 +24,7 @@ public class Controller {
 	public Controller () throws Exception
 	{
 		view = new MVCView();
-		modelo = new MVCModelo("./data/bogota-cadastral-2018-1-All-HourlyAggregate.cvs");
+		
 	}
 		
 	public void run() 
@@ -39,15 +40,151 @@ public class Controller {
 			int option = lector.nextInt();
 			switch(option){
 				case 1:
-					System.out.println("--------- \nDar hora inicial para cluster: ");
-				    int hora = lector.nextInt();
-				    Queue cluster=modelo.clusterMasG(hora);
-					System.out.println("Cluster más grande:");
-					for(int i=0;i<cluster.size();i++)
-					{ Viaje viaje= cluster.deQueue();
-					System.out.println("--------- \nElemento: "+i + ", Hora:" + viaje.darHora() +"\n---------");						
-					}break;
+					System.out.println("--------- \nDar trimestre a utilizar para la carga: ");
+				    int trimestre = lector.nextInt();
+				try {
+					modelo= new MVCModelo(trimestre);
+				} catch (Exception e) {
+					System.out.println("No se pudo realizar correctamente la carga");
+				}
+					
+					break;
 				case 2: 
+					System.out.println("--------- \n Dar zona de origen para la consulta del tiempo promedio y desviación estandar de viajes en base al mes "); 
+					 int zonaOM = lector.nextInt();
+					 System.out.println("--------- \n Dar zona de destino"); 
+					 int zonaDM = lector.nextInt();
+					 System.out.println("--------- \n Dar mes a consultar");
+					 int mesconsulta=lector.nextInt();
+				try {
+					Queue<Node<String>> consultaM=modelo.consultarTiempoPromedioYDesviacionMes(zonaOM, zonaDM, mesconsulta);
+					while(!consultaM.isEmpty()){
+						System.out.println(consultaM.deQueue());
+					}
+				} catch (Exception e) {
+					
+					System.out.println(e.getMessage());
+				}
+					break;	
+				case 3: 
+					System.out.println("--------- \n Dar numero N para la consulta de la información de los N viajes con mayor tiempo promedio para un mes dado"); 
+					 int NM= lector.nextInt();
+					 System.out.println("--------- \n Dar mes a consultar");
+					 int mesnumero=lector.nextInt();
+				try {
+					Queue<Node<String>> consultaMN=modelo.MayorTiempoPromedioNViajesMes(NM, mesnumero);
+					while(!consultaMN.isEmpty()){
+						System.out.println(consultaMN.deQueue());
+					}
+				} catch (Exception e) {
+					
+					System.out.println(e.getMessage());
+				}
+					
+					break;	
+				case 4: 
+					System.out.println("--------- \n Hasta pronto !! \n---------"); 
+					
+					
+					break;	
+				case 5: 
+					System.out.println("--------- \n Dar zona de origen para la consulta del tiempo promedio y desviación estandar de viajes en base al dia "); 
+					 int zonaOD = lector.nextInt();
+					 System.out.println("--------- \n Dar zona de destino"); 
+					 int zonaDD = lector.nextInt();
+					 System.out.println("--------- \n Dar mes a consultar");
+					 int diaconsulta=lector.nextInt();
+				try {
+					Queue<Node<String>> consultaD=modelo.consultarTiempoPromedioYDesviacionDia(zonaOD, zonaDD, diaconsulta);
+					while(!consultaD.isEmpty()){
+						System.out.println(consultaD.deQueue());
+					}
+				} catch (Exception e) {
+					
+					System.out.println(e.getMessage());
+				}
+					break;	
+				case 6: 
+					System.out.println("--------- \n Dar numero N para la consulta de la información de los N viajes con mayor tiempo promedio para un dia dado"); 
+					 int ND= lector.nextInt();
+					 System.out.println("--------- \n Dar mes a consultar");
+					 int dianumero=lector.nextInt();
+				try {
+					Queue<Node<String>> consultaDN=modelo.MayorTiempoPromedioNViajesDia(ND, dianumero);
+					while(!consultaDN.isEmpty()){
+						System.out.println(consultaDN.deQueue());
+					}
+				} catch (Exception e) {
+					
+					System.out.println(e.getMessage());
+				}
+				case 7: 
+					System.out.println("--------- \n Dar zona menor del rango para comparar los tiempos promedios de los viajes para una zona dada contra cada zona X en un rango de zonas dado en ambos sentidos para un día dado"); 
+					 int zonaMenorD= lector.nextInt();
+					 System.out.println("--------- \n Dar zona mayor");
+					 int zonaMayorD=lector.nextInt();
+					 System.out.println("--------- \n Dar zona a comparar");
+					 int zonaXD=lector.nextInt();
+					 System.out.println("--------- \n Dar dia dado");
+					 int diaZonas=lector.nextInt();
+				try {
+					Queue<Node<String>> consultaDZ=modelo.compararTiemposZonaDadaDia(zonaMenorD, zonaMayorD, zonaXD, diaZonas);
+					while(!consultaDZ.isEmpty()){
+						System.out.println(consultaDZ.deQueue());
+					}
+				} catch (Exception e) {
+					
+					System.out.println(e.getMessage());
+				}
+				case 8: 
+					System.out.println("--------- \n Dar zona de origen para la consulta de los viajes entre una zona de origen y una zona destino en una franja horaria dada."); 
+					 int zonaOH= lector.nextInt();
+					 System.out.println("--------- \n Dar zona de destino");
+					 int zonaDH=lector.nextInt();
+					 System.out.println("--------- \n Dar hora inicial");
+					 int horaI=lector.nextInt();
+					 System.out.println("--------- \n Dar hora final");
+					 int horaF=lector.nextInt();
+				try {
+					Queue<Node<String>> consultaHF=modelo.compararViajesFranjaHoraria(zonaOH, zonaDH, horaI, horaF);
+					while(!consultaHF.isEmpty()){
+						System.out.println(consultaHF.deQueue());
+					}
+				} catch (Exception e) {
+					
+					System.out.println(e.getMessage());
+				}
+				case 9: 
+					System.out.println("--------- \n Dar número N para la consulta de la información de los N viajes con mayor tiempo promedio para una hora dada"); 
+					 int NH= lector.nextInt();
+					 System.out.println("--------- \n Dar hora dada");
+					 int horaN=lector.nextInt();
+					
+				try {
+					Queue<Node<String>> consultaHN=modelo.MayorTiempoPromedioNViajesHora(NH, horaN);
+					while(!consultaHN.isEmpty()){
+						System.out.println(consultaHN.deQueue());
+					}
+				} catch (Exception e) {
+					
+					System.out.println(e.getMessage());
+				}
+				case 10: 
+					System.out.println("--------- \n Dar zona de origen para generar una gráfica ASCII que muestre el tiempo promedio de los viajes entre una zona origen y una zona destino para cada hora del día. "); 
+					 int zonaOASCII= lector.nextInt();
+					 System.out.println("--------- \n Dar zona de destino");
+					 int zonaDASCII=lector.nextInt();
+					
+				try {
+					Queue<Node<String>> consultaASCII=modelo.ASCIIZonas(zonaOASCII, zonaDASCII);
+					while(!consultaASCII.isEmpty()){
+						System.out.println(consultaASCII.deQueue());
+					}
+				} catch (Exception e) {
+					
+					System.out.println(e.getMessage());
+				}
+				case 11: 
 					System.out.println("--------- \n Hasta pronto !! \n---------"); 
 					lector.close();
 					fin = true;

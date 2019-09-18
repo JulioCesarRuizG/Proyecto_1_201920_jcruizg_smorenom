1f2316abeb7b7339f968b50b58fc3e9cc9ed30ae
Julio César Ruiz Gómez <jc.ruizg@uniandes.edu.co>
2019-09-17 21:24:55 -0500

    Se agrega docs

--- src/view/MVCView.java
+++ src/view/MVCView.java
@@ -14,13 +14,15 @@ public class MVCView
 	    
 		public void printMenu()
 		{
-			System.out.println("1. Crear Arreglo Dinamico de Strings");
-			System.out.println("2. Agregar String");
-			System.out.println("3. Buscar String");
-			System.out.println("4. Eliminar String");
-			System.out.println("5. Imprimir el Arreglo");
-			System.out.println("6. Exit");
-			System.out.println("Dar el numero de opcion a resolver, luego oprimir tecla Return: (e.g., 1):");
+			System.out.println("1. Consultar el tiempo promedio de viaje y desviación estándar de los viajes entre unas zonas - mes");
+			System.out.println("2. Consultar los N viajes con mayor tiempo promedio - mes");
+			System.out.println("3. Comparar los tiempo promedios de los viajes para una zona dada contra cada zona X en un rango - mes");
+			System.out.println("4. Consultar el tiempo promedio de viaje y desviación estándar de los viajes entre unas zonas - día");
+			System.out.println("5. Consultar los N viajes con mayor tiempo promedio - día");
+			System.out.println("6. Comparar los tiempo promedios de los viajes para una zona dada contra cada zona X en un rango - día");
+			System.out.println("7. Consultar el tiempo promedio de viaje y desviación estándar de los viajes entre unas zonas y una franja horaria - hora");
+			System.out.println("8. Consultar la información de N viajes con mayor tiempo promedio para una hora dada");
+			System.out.println("9. Generar una gráfica ASCII que muestre el tiempo promedio de los viajes entre una zona origen y destino. Cada hora de es representado con un *");
 		}
 
 		public void printMessage(String mensaje) {
